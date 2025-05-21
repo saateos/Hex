943939066545543d72ad0ff243230c7e33e9a2e1
Edoid <61113397+saateos@users.noreply.github.com>
2025-05-22 05:01:26 +0700

    collapse
    сделал коллапс столбцов и анимацию
    работает, если запускается в процессе постоянно, не уверен, хорошо ли это

--- Scripts/hex.gd
+++ Scripts/hex.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 @export var hex_type: String
 
-func dim():
-	var hex = get_node("Hex")
-	hex.modulate.a = .5
+func move(target):
+	var tween:Tween = create_tween()
+	tween.tween_property(self, "position", target, .5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN).set_delay(0.3)
+	pass
 func disappear():
 	var tween:Tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(0,0), .5).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)
